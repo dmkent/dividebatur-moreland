62a9e3f90ffe613273c4401f58aeba85482baa6e
David Kent <davidkent@fastmail.com.au>
2016-10-23 20:14:38 +1100

    remote aec data - not needed here.

--- aec_data/fed2016/nt/update.sh
+++
@@ -1,2 +0,0 @@
-curl -O http://vtr.aec.gov.au/External/aec-senate-formalpreferences-20499-NT.zip
-curl -O http://aec.gov.au/election/files/2016federalelection-all-candidates-nat-30-06-924.csv
--- aec_data/fed2016/tas/update.sh
+++
@@ -1,2 +0,0 @@
-curl -O http://vtr.aec.gov.au/External/aec-senate-formalpreferences-20499-NT.zip
-curl -O http://vtr.aec.gov.au/External/aec-senate-formalpreferences-20499-TAS.zip
